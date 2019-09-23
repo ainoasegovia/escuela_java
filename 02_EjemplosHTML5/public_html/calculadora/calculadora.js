369aca4b136f8227f8118d188956748b10f30e94
Ainoa <ainoa.segovia.daw@gmail.com>
2019-09-23 15:13:15 +0200

    ejemplo de callback y calculadora

--- 02_EjemplosHTML5/public_html/calculadora/calculadora.js
+++ 02_EjemplosHTML5/public_html/calculadora/calculadora.js
@@ -1,8 +1,40 @@
-    let result = document.getElementById("resultado");
-            
-    function pulsado(valor) {
-        if (valor ==="-")
-            result.value = - (parseFloat(result.value));
-	    else 
-            result.value += valor;
-    }
+class Calculadora {
+	
+	constructor(){
+		this.result = document.getElementById("resultado");
+	};
+	
+	numeroPulsado(evenObj){ // Funcion añadida como metodo
+		let valor = evenObj.currentTarget.innerHTML;
+		//alert("Pulsado: " + valor);
+		if (valor === "+/-"){
+			this.result.value = "" + (- (parseFloat(this.result.value)));
+		}else if(valor === "."){
+			if( ! calculadora.result.value.includes("."))
+				this.result.value += valor;
+		}else{
+			this.result.value += valor;
+		}
+	};
+
+};
+
+let calculadora = null;
+
+window.onload= function(){
+	let botones = document.getElementsByClassName("num"); // Obtener todos los elementos cuya clase sea "num", devuelve array
+	calculadora = new Calculadora();
+	
+	for(let boton of botones){
+		boton.addEventListener("click",(evtObj) => {
+			calculadora.numeroPulsado(evtObj);
+		});
+	}
+};
+
+/*calculadora.numeroPulsado = function(valor) { // Funcion añadida como metodo
+    if (valor === "-")
+        result.value = - (parseFloat(result.value));
+	else 
+        result.value += valor;
+}*/
