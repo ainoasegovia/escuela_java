f1e9e906f71a2e84fb447b3f3726b54720894876
Ainoa <ainoa.segovia.daw@gmail.com>
2019-10-02 10:17:13 +0200

    vista JSTL

--- 05_CRUD_Modelo_DAO/src/sinensia/model/logic/UserService.java
+++ 05_CRUD_Modelo_DAO/src/sinensia/model/logic/UserService.java
@@ -7,22 +7,23 @@ package sinensia.model.logic;
 
 import java.sql.SQLException;
 import java.util.List;
-import sinensia.model.User;
-import sinensia.model.persistence.IUserDAO;
+import sinensia.modelo.User;
+import sinensia.modelo.persistence.IUserDAO;
+import sinensia.modelo.persistence.UserDAO_DerbyDB;
 
 /**
  *
  * @author alumno
  */
 public class UserService {
-    
+
     IUserDAO daoUsers;
-    
+
     public UserService(IUserDAO daoUsers) {
         this.daoUsers = daoUsers;
     }
 
-	public boolean validate(String email, String password, String name, String strEdad) {
+    public boolean validate(String email, String password, String name, String strEdad) {
         if (email != null && password != null && name != null && strEdad != null) {
             if (!strEdad.matches("[0-9]{1,3}")) {
                 throw new IllegalArgumentException("La edad no es un número válido");
@@ -42,45 +43,41 @@ public class UserService {
 
     public User create(String email, String password, String name, String strEdad) throws SQLException {
         User u = null;
-		
         if (validate(email, password, name, strEdad)) {
-			
             int edad = Integer.parseInt(strEdad);
-		
             u = new User(email, password, name, edad);
             u = daoUsers.create(u);
         }
         return u;
     }
-	
+
     public List<User> getAll() throws SQLException {
         return daoUsers.getAll();
     }
-	
-    public boolean remove(int id)throws SQLException {
+
+    public boolean remove(int id) throws SQLException {
         return daoUsers.remove(id);
     }
-	
-	public User update(User user)throws SQLException {
-		if(validate(user.getEmail(), user.getPassword(), 
-				user.getName(), Integer.toString(user.getAge()))){
-			return daoUsers.update(user);
-		}else{
-			return null;
-		}
-	}
-	
-   public User update(int id, String email, String password, String name, String strEdad) throws SQLException {
+
+    public User update(User user) throws SQLException {
+        
+        if (validate(user.getEmail(), user.getPassword(), user.getName(), 
+                Integer.toString(user.getAge()))) {
+            return daoUsers.update(user);
+        } else {
+            return null;
+        }
+    }
+
+    public User update(int id, String email, String password, String name, String strEdad) throws SQLException {
         User u = null;
-		
         if (validate(email, password, name, strEdad)) {
-			
             int edad = Integer.parseInt(strEdad);
-			
             u = new User(email, password, name, edad);
-			u.setId(id);
+            u.setId(id);
             u = daoUsers.update(u);
         }
         return u;
     }
+	
 }
--- 05_CRUD_Modelo_DAO/test/sinensia/tests/ProbarModeloDAO.java
+++ 05_CRUD_Modelo_DAO/test/sinensia/tests/ProbarModeloDAO.java
@@ -18,9 +18,9 @@ import static org.junit.Assert.*;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
 import sinensia.model.logic.UserService;
-import sinensia.model.User;
-import sinensia.model.persistence.IUserDAO;
-import sinensia.model.persistence.UserDAO_DerbyDB;
+import sinensia.modelo.User;
+import sinensia.modelo.persistence.IUserDAO;
+import sinensia.modelo.persistence.UserDAO_DerbyDB;
 
 /**
  *
@@ -47,132 +47,112 @@ public class ProbarModeloDAO {
     @Test
     public void TestModel_01_createAndListUsersFail() {
         List<User> allUsers;
-		
         try {
             allUsers = userSrv.getAll();
-			
             int totalUsersBefore = allUsers.size();
-			
             User u1 = userSrv.create(null, null, null, "0");
             User u2 = userSrv.create("", "p1234", "J J", "20");
             User u3 = userSrv.create("er@dd", null, "J J", "20");
             User u4 = userSrv.create("er@dd", "p1234", "", "2");
             User u5 = userSrv.create("er@dd", "p1234", "J J", "0");
-			
             assertNull(u1);
             assertNull(u2);
             assertNull(u3);
             assertNull(u4);
             assertNull(u5);
-			
             allUsers = userSrv.getAll();
             assertEquals(totalUsersBefore, allUsers.size());
         } catch (SQLException ex) {
             Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);
             fail("Error en SQL: " + ex.getMessage());
         }
-        
+
     }
 
     @Test
     public void TestModel_02_createAndListUsersOK() {
         try {
             List<User> allUsers = userSrv.getAll();
-			
             int totalUsersBefore = allUsers.size();
-			
             User u1 = userSrv.create("aaa@mail.com", "a1234", "Aaaaa", "20");
             User u2 = userSrv.create("bbb@mail.com", "b1234", "B. Bbbb", "30");
             User u3 = userSrv.create("ccc@mail.com", "c1234", "Cccc C.", "40");
             User u4 = userSrv.create("ddd@mail.com", "d1234", "D. D. Dd", "50");
-			
             assertNotNull(u1);
             assertEquals("B. Bbbb", u2.getName());
             assertTrue("ccc@mail.com".equals(u3.getEmail()));
             assertFalse(u4.getAge() != 50);
-            
+
             // Repetido que debe fallar
             try {
                 User u5 = userSrv.create("ddd@mail.com", "d1234", "D. D. Dd", "50");
                 fail("No debe crearse usuario, está duplicado");
-            } catch (Exception e) {                
+            } catch (Exception e) {
             }
-            
+
             allUsers = userSrv.getAll();
             assertEquals(totalUsersBefore + 4, allUsers.size());
-            
+
             // Eliminar los 4 usuarios creados:
             //   Debemos devolver el Id en el objeto usuario creado
             //   Para poder devolver el Id habrá que preguntar a la bbdd por email
             //   Y entonces ya podremos eliminar por Id. Incluso por email...
-			
             userSrv.remove(u1.getId());
             userSrv.remove(u2.getId());
             userSrv.remove(u3.getId());
             userSrv.remove(u4.getId());
-            
+
             allUsers = userSrv.getAll();
             assertEquals(totalUsersBefore, allUsers.size());
             // Comprobar que se han eliminado
-            
+
         } catch (SQLException ex) {
             Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);
             fail("Error en SQL: " + ex.getMessage());
         }
     }
-	
-	@Test
-    public void TestModel_03_createAndListUsersOK() {
+
+    @Test
+    public void TestModel_03_createAndUpdateUsersOK() {
         try {
-			
             User u1 = userSrv.create("aaa@mail.com", "a1234", "Aaaaa", "20");
             User u2 = userSrv.create("bbb@mail.com", "b1234", "B. Bbbb", "30");
-			
-			u1.setName("AAA AAA");
-			
-			u1 = userSrv.update(u1);
-			
-			u2 = userSrv.update(u2.getId(),"BBB@mail.com","1234BBBB",u2.getName(),"99");
-			
-			assertEquals("AAA AAA", u1.getName());
-			
-			assertEquals("BBB@mail.com", u2.getEmail());
-			
-			assertEquals("1234BBBB", u2.getPassword());
-			
-			assertEquals(99, u2.getAge());
+            u1.setName("AAA AAA");
+            u1 = userSrv.update(u1);
+            u2 = userSrv.update(u2.getId(), "BBB@mail.com", "1234BBBB", u2.getName(), "99");
 
-			userSrv.remove(u1.getId());
-			userSrv.remove(u2.getId());
-			
-		}catch (SQLException ex) {
+            assertEquals("AAA AAA", u1.getName());
+            assertEquals("BBB@mail.com", u2.getEmail());
+            assertEquals("1234BBBB", u2.getPassword());
+            assertEquals(99, u2.getAge());
+            userSrv.remove(u1.getId());
+            userSrv.remove(u2.getId());
+        } catch (SQLException ex) {
             Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);
             fail("Error en SQL: " + ex.getMessage());
         }
-	}
-	
-	@Test
-    public void TestModel_04_createAndListUsersFail() {
-		User u1 = null;
-		User u2 = null;
+    }
+    
+    @Test
+    public void TestModel_04_createAndUpdateUsersFail() {
+        User u1 = null;
+        User u2 = null;
         try {
-			
             u1 = userSrv.create("aaa@mail.com", "a1234", "Aaaaa", "20");
             u2 = userSrv.create("bbb@mail.com", "b1234", "B. Bbbb", "30");
-			
-			// Debe fallar
-			u2 = userSrv.update(u2.getId(),"aaa@mail.com","1234BBBB",u2.getName(),"99");
-            fail("Error no ha fallado la clave email duplicado");
-
-		}catch (SQLException ex) {
-            Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);
+            // Debe fallar
+            u2 = userSrv.update(u2.getId(), "aaa@mail.com", "1234BBBB", u2.getName(), "99");
+            fail("Error, no ha fallado la clave email duplicada");
+        }catch (SQLException ex) {
+            Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);            
         }
-		try{
-					
-			userSrv.remove(u1.getId());
-			userSrv.remove(u2.getId());
-		}catch (SQLException ex) {
+        try {
+            userSrv.remove(u1.getId());
+            userSrv.remove(u2.getId());
+        } catch (SQLException ex) {
             Logger.getLogger(ProbarModeloDAO.class.getName()).log(Level.SEVERE, null, ex);
+            fail("Error en SQL: " + ex.getMessage());
         }
-	}
+    }
 }
+
