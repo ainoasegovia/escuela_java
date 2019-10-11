9e0ac0330aede0b05ce1ada64d8e4f4f5a6fc5ed
Ainoa <ainoa.segovia.daw@gmail.com>
2019-10-11 09:48:48 +0200

    cambio proyecto

--- 05_CRUD_Vista_JSTL/src/java/sinensia/controllers/apirest/UsersRestController.java
+++ 05_CRUD_Vista_JSTL/src/java/sinensia/controllers/apirest/UsersRestController.java
@@ -1,137 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package sinensia.controllers.apirest;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import java.io.BufferedReader;
 import sinensia.controllers.*;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javafx.scene.control.Alert;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.JOptionPane;
-import sinensia.model.User;
 import sinensia.model.logic.UserService;
-import sinensia.model.persistence.IUserDAO;
-import sinensia.model.persistence.UserDAO_DerbyBD;
+import sinensia.modelo.User;
+import sinensia.modelo.persistence.IUserDAO;
+import sinensia.modelo.persistence.UserDAO_DerbyDB;
 
 /**
- * @author Danny
+ *
+ * @author alumno
  */
-@WebServlet(asyncSupported = true, urlPatterns = "/api/users/*")
+@WebServlet(asyncSupported = true, urlPatterns = "/api/users")
 public class UsersRestController extends HttpServlet {
 
-	UserService userSrv;
+    UserService userSrv;
 
-	@Override
-	public void init() throws ServletException {
-		super.init();
-		IUserDAO usersDAO = new UserDAO_DerbyBD();
-		this.userSrv = new UserService(usersDAO);
-	}
+    @Override
+    public void init() throws ServletException {
+        super.init(); //To change body of generated methods, choose Tools | Templates.
 
-	@Override
-	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("application/json;charset=UTF-8");
-		String id = req.getQueryString();
-		//String jsonUser = req.getReader().readLine();
-		setAccessControlHeader(resp);
+        IUserDAO usersDAO = new UserDAO_DerbyDB();
+        this.userSrv = new UserService(usersDAO);
+    }
 
-		//User userObject = new Gson().fromJson(jsonUser, User.class);
-		try {
-			userSrv.delete(Integer.parseInt(id));
-		} catch (SQLException ex) {
-			Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
-		}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json;charset=UTF-8");
+        setAccessControlHeaders(resp);
 
-		String json = "{ \"borrado\": \"true\", \"mensaje\": EXITO }";
-		JsonObject jsonObject = new JsonParser().parse(json).getAsJsonObject();
+        try {
+            List<User> usersList = userSrv.getAll();
+            // Serializamos el List en un JSON
+            Gson gson = new Gson();
+            String textJson = gson.toJson(usersList);
+            // Devolverá [ {"id": 1, "email": "aaa@aaa.com"...},  ]
+            resp.getWriter().print(textJson);
+        } catch (Exception ex) {
+            Logger.getLogger(UsersRestController.class.getName()).log(Level.SEVERE, null, ex);
+            resp.getWriter().print("{\"error\": \""
+                    + ex.getMessage() + "\"}");
+        }
+    }
 
-		resp.getWriter().print(jsonObject);
-	}
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        setAccessControlHeaders(resp);
+        //Recicir el JSON como parámetro de FORMulario
+        //String jsonUser = req.getParameter("json");
+        BufferedReader bufRead = req.getReader();
+        String jsonUser;
+        jsonUser = bufRead.readLine();
+        /*String li
+         while (jsonUser != null) {
+             jsonUser += bu
+         }*/
+        Logger.getLogger(UsersRestController.class.getName()).log(Level.SEVERE, null, jsonUser);
 
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("application/json;charset=UTF-8");
-		String jsonUser = req.getReader().readLine();
-		setAccessControlHeader(resp);
-		//String jsonUser = req.getParameter("json");
-		//String jsonUser ="{'email':'dani@gmail.com','edad':'32'}";
+        User userObject = new Gson().fromJson(jsonUser, User.class);
+        try {
+            userObject = userSrv.create(
+                    userObject.getEmail(),
+                    userObject.getPassword(),
+                    userObject.getName(),
+                    Integer.toString(userObject.getAge()));
+            resp.setContentType("application/json;charset=UTF-8");
 
-		User userObject = new Gson().fromJson(jsonUser, User.class);
+            Gson gson = new Gson();
+            String textJson = gson.toJson(userObject);
+            resp.getWriter().print(textJson);
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersRestController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
-		try {
-			userObject = userSrv.create(
-					userObject.getEmail(),
-					userObject.getPassword(),
-					userObject.getName(),
-					userObject.getAge());
-		} catch (SQLException ex) {
-			Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
-		}
-
-		Gson gson = new Gson();
-		String textJson = gson.toJson(userObject);
-
-		resp.getWriter().print(textJson);
-
-	}
-
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		resp.setContentType("application/json;charset=UTF-8");
-		setAccessControlHeader(resp);
-		try {
-			List<User> userList = userSrv.getAll();
-			//serializamos el list en un JSON
-
-			Gson gson = new Gson();
-			String textJson = gson.toJson(userList);
-			//Devolvera [{"id":1, "email.....}, {},...]
-			resp.getWriter().print(textJson);
-		} catch (SQLException ex) {
-			Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
-			resp.getWriter().print("{\"error\": \" "
-					+ ex.getMessage() + "\"");
-		}
-	}
-	
-	    @Override
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+        setAccessControlHeaders(resp);
+            String jsonUser = req.getReader().readLine();
+            User userObject = new Gson().fromJson(jsonUser, User.class);
+           userSrv.remove(userObject.getId());
+            resp.getWriter().print("OK");
+        } catch (SQLException ex) {
+            Logger.getLogger(UsersRestController.class.getName()).log(Level.SEVERE, null, ex);
+  
+            resp.getWriter().print("ERROR: " + ex.getMessage());
+        }
+    }
+    
+    @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String jsonUser = req.getReader().readLine();
+        setAccessControlHeaders(resp);
         User userObject = new Gson().fromJson(jsonUser, User.class);
-		setAccessControlHeader(resp);
         try { // Debe venir ya con el id
-            userObject = userSrv.modify(userObject);
+            userObject = userSrv.update(userObject);
         } catch (SQLException ex) {
-            Logger.getLogger(UsersController.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UsersRestController.class.getName()).log(Level.SEVERE, null, ex);
         }
         resp.setContentType("application/json;charset=UTF-8");
         resp.getWriter().print(new Gson().toJson(userObject));
     }
 
+    
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        setAccessControlHeaders(resp);
+        resp.setStatus(HttpServletResponse.SC_OK); // Devolvemos cod 200 = OK
+    }
 
-	private void setAccessControlHeader(HttpServletResponse resp) {
-		resp.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-		resp.setHeader("Access-Control-Allow-Methods", "OPTIONS,HEAD,GET,PUT,DELETE,POST"); //get post put delete
-		resp.setHeader("Access-Control-Allow-Headers", "X-PINGOTHER,Origin,X-Requested-With,Content-Type,Accept");
-		resp.setHeader("Access-Control-Max-Age", "1728000");//20 dias
-	}
-
-	@Override
-	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-		setAccessControlHeader(resp);
-		resp.setStatus(HttpServletResponse.SC_OK);
-	}
-
+    private void setAccessControlHeaders(HttpServletResponse resp) {
+        resp.setHeader("Access-Control-Allow-Origin",
+                "http://localhost:4200");
+        
+        resp.setHeader("Access-Control-Allow-Methods",
+                "OPTIONS,HEAD,GET,POST,PUT,DELETE");
+        
+        resp.setHeader("Access-Control-Allow-Headers",
+                "X-PINGOTHER, Origin, X-Requested-With, Content-Type, Accept"); 
+        resp.addHeader("Access-Control-Max-Age", "1728000"); // 20 días
+    }
 }
--- 05_CRUD_Vista_JSTL/web/js/ajaxRestClientJQ.js
+++ 05_CRUD_Vista_JSTL/web/js/ajaxRestClientJQ.js
@@ -1,23 +1,23 @@
-$(document).ready( function() {
-    
-    $.ajax( {
+$(document).ready(function () {
+
+    $.ajax({
         "url": "http://localhost:8084/CRUD_Vista_JSTL/api/users",
         "type": "GET",
-        "success": function(respuestaArrayUsers) {
-            
-            respuestaArrayUsers.forEach(function(user) {
-                let htmlTRnuevaFila = $("#data_table").html() + 
-                    `<tr><td>${user.email}</td>
+        "success": function (respuestaArrayUsers) {
+
+            respuestaArrayUsers.forEach(function (user) {
+                let htmlTRnuevaFila = $("#data_table").html() +
+                        `<tr><td>${user.email}</td>
                         <td>${user.name}</td>
                     </tr>`;
                 $("#data_table").html(htmlTRnuevaFila);
             });
         },
-        "error": function() {
+        "error": function () {
             alert("Error en peticion AJAX");
         }
     });
-	
+
     $("#btnSend").click(function () {
         let objUser = {
             "email": $("#email").val(),
@@ -25,49 +25,48 @@ $(document).ready( function() {
             "name": $("#name").val(),
             "age": $("#age").val()
         };
-        $.post({"url": "api/users", 
+        $.post({"url": "api/users",
             "contentType": "application/json;charset=UTF-8",
             "data": JSON.stringify(objUser),
             "success":
-                function (respuestaUserObj) {
-                    $("#id").val(respuestaUserObj.id);
-                }
+                    function (respuestaUserObj) {
+                        $("#id").val(respuestaUserObj.id);
+                    }
         });
     });
-	
     $("#btnDel").click(function () {
-        $.ajax({"url": "api/users", 
-			"method": "DELETE",
-			"contentType": "application/json;charset=UTF-8",
-			// Enviamos como obj
-            "data": JSON.stringify({"id": $("#id").val()}), 
+        $.ajax({"url": "api/users",
+            "method": "DELETE",
+            "contentType": "application/json;charset=UTF-8",
+            // Enviamos como JSON
+            "data": JSON.stringify({ "id": $("#id").val() }), 
             "success":
-                function (respuestaText) {
-                    alert(respuestaText);
-                },
-			"error":
-                function (respuestaText) {
-                    alert(respuestaText);
-                }
+                    function (respuestaText) {
+                        alert(respuestaText);
+                    }, // Enviamos el obj como parámetros de formulario
+            "error":
+                    function (respuestaText) {
+                        alert(respuestaText);
+                    }
         });
-	});
-	
-	  $("#btnUpdate").click(function () {
+    });
+    
+    $("#btnUpdate").click(function () {
         let objUser = {
-			"id": $("#id").val(),
+            "id": $("#id").val(),
             "email": $("#email").val(),
             "password": $("#password").val(),
             "name": $("#name").val(),
             "age": $("#age").val()
         };
-        $.ajax({"url": "api/users", 
-			"method": "PUT",
+        $.ajax({"url": "api/users",
+            "method": "PUT",
             "contentType": "application/json;charset=UTF-8",
             "data": JSON.stringify(objUser),
             "success":
-                function (respuestaUserObj) {
-                    $("#id").val(respuestaUserObj.id);
-                }
+                    function (respuestaUserObj) {
+                        $("#id").val(respuestaUserObj.id);
+                    }
         });
-	});
+    });
 });
