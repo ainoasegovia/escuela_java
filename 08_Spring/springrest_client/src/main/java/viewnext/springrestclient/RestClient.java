aef57f424942a24f38ed621658b4a9dd99d2a737
Ainoa <ainoa.segovia.daw@gmail.com>
2019-10-11 17:00:37 +0200

    spring cliente

--- 08_Spring/springrest_client/src/main/java/viewnext/springrestclient/RestClient.java
+++ 08_Spring/springrest_client/src/main/java/viewnext/springrestclient/RestClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class RestClient {
 
-	private static final String URL_SRV_87 = "http://172.16.2.9:8087/users";
+	private static final String URL_SRV_87 = "http://172.16.2.9:8087";
 	private RestTemplate rest;
 	private HttpHeaders headers;
 	private HttpStatus status;
@@ -23,10 +23,22 @@ public class RestClient {
 
 	public String get(String uri) {
 		HttpEntity<String> requestEntity = new HttpEntity<String>("", headers);
-		ResponseEntity<String> responseEntity = rest.exchange(URL_SRV_87 + uri, 
-				HttpMethod.GET, 
-				requestEntity,
-				String.class);
+		ResponseEntity<String> responseEntity = 
+				rest.exchange(URL_SRV_87 + uri, 
+					HttpMethod.GET, 
+					requestEntity,
+					String.class);
+		this.status = responseEntity.getStatusCode();
+		return responseEntity.getBody();
+	}
+	
+	public String post(String uri,String jsonAEnviar) {
+		HttpEntity<String> requestEntity = new HttpEntity<String>(jsonAEnviar, headers);
+		ResponseEntity<String> responseEntity = 
+				rest.exchange(URL_SRV_87 + uri, 
+					HttpMethod.POST, 
+					requestEntity,
+					String.class);
 		this.status = responseEntity.getStatusCode();
 		return responseEntity.getBody();
 	}
--- 08_Spring/springrest_client/src/main/java/viewnext/springrestclient/SpringrestClientApplication.java
+++ 08_Spring/springrest_client/src/main/java/viewnext/springrestclient/SpringrestClientApplication.java
@@ -9,16 +9,22 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class SpringrestClientApplication {
-
-	private static final String URL_SRV_87 = "http://172.16.2.9:8087/users";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringrestClientApplication.class, args);
 		
 		System.out.println(">>> Recogiendo usuarios <<<");
-		
-		System.out.println(">>> Usuarios recogidos <<<");
-
+		RestClient clienteRest = new RestClient();
+		String jsonUsers = clienteRest.get("/users");
+		System.out.println(">>> Usuarios recogidos <<< \n" + jsonUsers);
+		String usuarioAEnviar = "{\n" + 
+				"    \"id\": 6,\n" + 
+				"    \"email\": \"aaaa@desde.com\",\n" + 
+				"    \"name\": \"Spring Rest Cli\",\n" + 
+				"    \"age\": 50\n" + 
+				"}";
+		String jsonUserCreado = clienteRest.post("/users", usuarioAEnviar);
+		System.out.println(">>> Usuarios creado <<< \n" + jsonUserCreado);
 	}
 
 	/*public List<User> getTodosUsuarios(){
