ee5fb2f44b845584158861a58189e6bae0a836d1
Ainoa <ainoa.segovia.daw@gmail.com>
2019-10-11 16:41:27 +0200

    spring

---
+++ 08_Spring/springrest_client/src/main/java/viewnext/springrestclient/SpringrestClientApplication.java
@@ -0,0 +1,28 @@
+package viewnext.springrestclient;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringrestClientApplication {
+
+	private static final String URL_SRV_87 = "http://172.16.2.9:8087/users";
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestClientApplication.class, args);
+		
+		System.out.println(">>> Recogiendo usuarios <<<");
+		
+		System.out.println(">>> Usuarios recogidos <<<");
+
+	}
+
+	/*public List<User> getTodosUsuarios(){
+		
+		
+	}*/
+}
